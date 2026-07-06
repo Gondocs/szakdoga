e99fa71064aa455a7fa47eaaf0190a66bea652ba
Gondocs Robert <gondocs.robert@gmail.com>
2026-07-07 01:00:50 +0200

    refactor Providers component to wrap LocalizationProvider with FontScaleProvider

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -5,16 +5,19 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { hu } from 'date-fns/locale';
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from '../features/auth/AuthContext';
+import { FontScaleProvider } from '../features/settings/FontScaleContext';
 import { muiTheme } from '../theme/muiTheme';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider theme={muiTheme}>
       <CssBaseline />
-      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={hu}>
-        <AuthProvider>{children}</AuthProvider>
-        <ToastContainer position="bottom-right" theme="light" autoClose={4000} newestOnTop />
-      </LocalizationProvider>
+      <FontScaleProvider>
+        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={hu}>
+          <AuthProvider>{children}</AuthProvider>
+          <ToastContainer position="bottom-right" theme="light" autoClose={4000} newestOnTop />
+        </LocalizationProvider>
+      </FontScaleProvider>
     </ThemeProvider>
   );
 }
