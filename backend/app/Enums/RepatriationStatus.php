0a1fe203238e28a288fccf82186516c1e1bbd415
Gondocs Robert <gondocs.robert@gmail.com>
2026-07-10 01:46:57 +0200

    add label method to RepatriationStatus enum for localized representation

--- backend/app/Enums/RepatriationStatus.php
+++ backend/app/Enums/RepatriationStatus.php
@@ -12,4 +12,13 @@ enum RepatriationStatus: string
     case NotPermitted = 'not_permitted';
     case Conditional = 'conditional';
     case Permitted = 'permitted';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::NotPermitted => 'Nem engedélyezett',
+            self::Conditional => 'Feltételes',
+            self::Permitted => 'Engedélyezett',
+        };
+    }
 }
