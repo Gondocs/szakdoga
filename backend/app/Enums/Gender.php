ff4f1a884dcf059022d15a3c6074a3cb1fa1004d
Gondocs Robert <gondocs.robert@gmail.com>
2026-07-10 01:46:45 +0200

    add label method to Gender enum for localized gender representation

--- backend/app/Enums/Gender.php
+++ backend/app/Enums/Gender.php
@@ -7,4 +7,13 @@ enum Gender: string
     case Male = 'male';
     case Female = 'female';
     case Other = 'other';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Male => 'Férfi',
+            self::Female => 'Nő',
+            self::Other => 'Egyéb',
+        };
+    }
 }
