21d3811500284111c12a753d445f316533221efe
Gondocs Robert <gondocs.robert@gmail.com>
2026-07-17 00:01:59 +0200

    feat: extend Mail functionality with Mailtrap transport configuration

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Mailer\Bridge\Mailtrap\Transport\MailtrapApiTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Mail::extend('mailtrap', fn (array $config) => new MailtrapApiTransport($config['key'] ?? env('MAILTRAP_KEY')));
     }
 }
